ac3b763cdb046f0dfc2dd980a8bc617670e74e86
Be30nCH1k <150174503+Be30nCH1k@users.noreply.github.com>
2024-09-23 22:08:32 +0300

    Add files via upload

---
+++ FahionСommand/question/assets/Js/question.js
@@ -0,0 +1,12 @@
+
+function openForm(){
+    form.style.display = "block";
+    form.style.transition = " 0.5s";
+}
+
+function closeForm(){
+    form.style.display = "none";
+}
+
+btn.onclick = openForm;
+btnClose.onclick = closeForm; 
